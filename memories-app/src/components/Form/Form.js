fbbe50f77e532a657449d44a82776254c8c7feed
Thanh Le <thanhlevan0203@gmail.com>
2022-07-03 22:37:47 -0700

    New  updated App.js

--- memories-app/src/components/Form/Form.js
+++ memories-app/src/components/Form/Form.js
@@ -3,7 +3,7 @@ import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector} from 'react-redux';
 import FileBase from 'react-file-base64';
 
-import useStyle from './styles';
+import useStyles from './styles';
 import { createPost, updatePost } from '../..actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
